b5d37c3f504852af96ced46d22e43baa23de1dc0
Lina <lsabogalarias@gmail.com>
2023-08-11 11:09:45 -0500

    Agregando archivos iniciales

---
+++ database/migrations/2023_08_11_160449_create_lines_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lines', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->integer('qty');
+            $table->unsignedBigInteger('product_id');            
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_lines');
+    }
+}
