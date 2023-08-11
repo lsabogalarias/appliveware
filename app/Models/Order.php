0bc82285cf3e40b4aa566745b60c8fbe5d4439d6
Lina <lsabogalarias@gmail.com>
2023-08-11 11:42:05 -0500

    Creacion de modelos y ejecucion de migraciones en base de datos

---
+++ app/Models/Order.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['order_ref', 'customer_name'];
+    
+    public function orderLines(){
+        return $this->hasMany(OrderLine::class);
+    }
+}
